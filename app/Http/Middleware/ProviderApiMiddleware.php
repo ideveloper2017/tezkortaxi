ab7414aa239dd3ebf1b5dbaa7fb1a50c1278faf3
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-12-04 11:23:25 +0500

    Commit

--- app/Http/Middleware/ProviderApiMiddleware.php
+++ app/Http/Middleware/ProviderApiMiddleware.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Config;
 use Closure;
-
-use JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Exceptions\TokenInvalidException;
-use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Illuminate\Http\Request;
 
 class ProviderApiMiddleware
 {
@@ -16,63 +11,20 @@ class ProviderApiMiddleware
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-//    public function handle($request, Closure $next)
-//    {
-//        Config::set('auth.providers.users.model', 'App\Models\Provider');
-//
-//        try {
-//
-//            if (!$user = JWTAuth::parseToken()->authenticate()) {
-//                return response()->json(['user_not_found'], 404);
-//            } else {
-//                \Auth::loginUsingId($user->id);
-//            }
-//
-//        } catch (TokenExpiredException $e) {
-//
-//            return response()->json(['error' => 'token_expired'], $e->getStatusCode());
-//
-//        } catch (TokenInvalidException $e) {
-//
-//            return response()->json(['error' => 'token_invalid'], $e->getStatusCode());
-//
-//        } catch (JWTException $e) {
-//
-//            return response()->json(['error' => 'token_absent'.' '.$e->getLine().' '.$e->getMessage()]);
-//
-//        }
-//
-//        return $next($request);
-//    }
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
-            Auth::setUser($user);
-        }
-        catch (TokenExpiredException $e) {
-
-            return response()->json([
-                'error' => 'Token Expired!',
-                'statusCode' => (int)401
-            ], 401);
-
-        } catch (TokenInvalidException $e) {
-            return response()->json([
-                'error' => 'Not Authorized!',
-                'statusCode' => $e->getFile().' '.$e->getCode()
-            ], 401);
-
+            if (!$user) {
+                return response()->json(['message' => 'user not found'], 500);
+            }
         } catch (JWTException $e) {
-            return response()->json([
-                'error' => 'Not Authorized!',
-                'statusCode' => (int)401
-            ], 401);
+            return response()->json(['message' => $e->getMessage()], 500);
         }
-
         return $next($request);
+
     }
 }

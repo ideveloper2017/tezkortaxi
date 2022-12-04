5e599e35ed809a34cbaba1c7c90d21492b8134e7
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-12-05 01:18:17 +0500

    Commit

--- app/Http/Controllers/ProviderResources/TripController.php
+++ app/Http/Controllers/ProviderResources/TripController.php
@@ -238,8 +238,7 @@ class TripController extends Controller
     public function scheduled(Request $request)
     {
         try{
-            $Jobs = UserRequests::where('provider_id', Auth::user()->id)
-                    ->where('status', 'SCHEDULED')
+            $Jobs = UserRequests::where('status', 'SCHEDULED')
                     ->with('service_type')
                     ->get();
 

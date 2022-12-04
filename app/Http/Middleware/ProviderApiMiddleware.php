f9f68c792eaa10889f51d887ff671ff69825ae55
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-12-04 13:13:27 +0500

    Commit

--- app/Http/Middleware/ProviderApiMiddleware.php
+++ app/Http/Middleware/ProviderApiMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use JWTAuth;
+use Tymon\JWTAuth\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
@@ -20,14 +20,22 @@ class ProviderApiMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        try {
-            $user = JWTAuth::parseToken()->authenticate(false,'provider');
-            if (!$user) {
-                return response()->json(['message' => 'user not found'], 500);
+
+        dd($request->token);
+//        try {
+//            $user = JWTAuth::parseToken()->authenticate();
+//            if (!$user) {
+//                return response()->json(['message' => 'user not found'], 500);
+//            }
+//        } catch (JWTException $e) {
+//            return response()->json(['message' => $e->getMessage()], 500);
+//        }
+
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            } else {
+                \Auth::loginUsingId($user->id);
             }
-        } catch (JWTException $e) {
-            return response()->json(['message' => $e->getMessage()], 500);
-        }
         return $next($request);
 
     }
